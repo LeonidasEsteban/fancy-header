c30aebe9c1f33302b9e145d282a695127634f803
Leonidas Esteban <leonidasesteban@gmail.com>
2019-03-30 13:23:46 -0600

    ups

--- src/App.js
+++ src/App.js
@@ -1,137 +1,132 @@
 import React from 'react'
-
 import Header from './header'
 
-
 function App() {
-
   return (
-    <>
-      <Header
-        menuList={[
-          {
-            label: 'option1',
-            url: 'https://ongrin.com/somwthing',
-          },
-          {
-            label: 'option2',
-            url: 'https://ongrin.com/somwthing',
-          },
-          {
-            label: 'option3',
-            url: 'https://ongrin.com/somwthing',
-          },
-          {
-            label: 'option4',
-            url: 'https://ongrin.com/somwthing',
-          },
-          {
-            label: 'option5',
-            url: 'https://ongrin.com/somwthing',
-          },
-          {
-            label: 'option6',
-            url: 'https://ongrin.com/somwthing',
-          },
-          {
-            label: 'option7',
-            url: 'https://ongrin.com/somwthing',
-          },
-          {
-            label: 'option8',
-            url: 'https://ongrin.com/somwthing',
-          },
-          {
-            label: 'option9',
-            url: 'https://ongrin.com/somwthing',
-          },
-          {
-            label: 'option10',
-            url: 'https://ongrin.com/somwthing',
-          },
-          {
-            label: 'option11',
-            url: 'https://ongrin.com/somwthing',
-          },
-          {
-            label: 'option12',
-            url: 'https://ongrin.com/somwthing',
-          },
-          {
-            label: 'option13',
-            url: 'https://ongrin.com/somwthing',
-          },
-          {
-            label: 'option14',
-            url: 'https://ongrin.com/somwthing',
-          },
-          {
-            label: 'option15',
-            url: 'https://ongrin.com/somwthing',
-          },
-          {
-            label: 'option16',
-            url: 'https://ongrin.com/somwthing',
-          },
-          {
-            label: 'option17',
-            url: 'https://ongrin.com/somwthing',
-          },
-          {
-            label: 'option18',
-            url: 'https://ongrin.com/somwthing',
-          },
-          {
-            label: 'option19',
-            url: 'https://ongrin.com/somwthing',
-          },
-          {
-            label: 'option20',
-            url: 'https://ongrin.com/somwthing',
-          },
-          {
-            label: 'option21',
-            url: 'https://ongrin.com/somwthing',
-          },
-          {
-            label: 'option22',
-            url: 'https://ongrin.com/somwthing',
-          },
-          {
-            label: 'option23',
-            url: 'https://ongrin.com/somwthing',
-          },
-          {
-            label: 'option24',
-            url: 'https://ongrin.com/somwthing',
-          },
-          // {
-          //   label: 'option25',
-          //   url: 'https://ongrin.com/somwthing',
-          // },
-          // {
-          //   label: 'option26',
-          //   url: 'https://ongrin.com/somwthing',
-          // },
-          // {
-          //   label: 'option27',
-          //   url: 'https://ongrin.com/somwthing',
-          // },
-          // {
-          //   label: 'option28',
-          //   url: 'https://ongrin.com/somwthing',
-          // },
-          // {
-          //   label: 'option29',
-          //   url: 'https://ongrin.com/somwthing',
-          // },
-          // {
-          //   label: 'option30',
-          //   url: 'https://ongrin.com/somwthing',
-          // },
-        ]}
-      />
-    </>
+    <Header
+      menuList={[
+        {
+          label: 'option1',
+          url: 'https://ongrin.com/somwthing',
+        },
+        {
+          label: 'option2',
+          url: 'https://ongrin.com/somwthing',
+        },
+        {
+          label: 'option3',
+          url: 'https://ongrin.com/somwthing',
+        },
+        {
+          label: 'option4',
+          url: 'https://ongrin.com/somwthing',
+        },
+        {
+          label: 'option5',
+          url: 'https://ongrin.com/somwthing',
+        },
+        {
+          label: 'option6',
+          url: 'https://ongrin.com/somwthing',
+        },
+        {
+          label: 'option7',
+          url: 'https://ongrin.com/somwthing',
+        },
+        {
+          label: 'option8',
+          url: 'https://ongrin.com/somwthing',
+        },
+        {
+          label: 'option9',
+          url: 'https://ongrin.com/somwthing',
+        },
+        {
+          label: 'option10',
+          url: 'https://ongrin.com/somwthing',
+        },
+        {
+          label: 'option11',
+          url: 'https://ongrin.com/somwthing',
+        },
+        {
+          label: 'option12',
+          url: 'https://ongrin.com/somwthing',
+        },
+        {
+          label: 'option13',
+          url: 'https://ongrin.com/somwthing',
+        },
+        {
+          label: 'option14',
+          url: 'https://ongrin.com/somwthing',
+        },
+        {
+          label: 'option15',
+          url: 'https://ongrin.com/somwthing',
+        },
+        {
+          label: 'option16',
+          url: 'https://ongrin.com/somwthing',
+        },
+        {
+          label: 'option17',
+          url: 'https://ongrin.com/somwthing',
+        },
+        {
+          label: 'option18',
+          url: 'https://ongrin.com/somwthing',
+        },
+        {
+          label: 'option19',
+          url: 'https://ongrin.com/somwthing',
+        },
+        {
+          label: 'option20',
+          url: 'https://ongrin.com/somwthing',
+        },
+        {
+          label: 'option21',
+          url: 'https://ongrin.com/somwthing',
+        },
+        {
+          label: 'option22',
+          url: 'https://ongrin.com/somwthing',
+        },
+        {
+          label: 'option23',
+          url: 'https://ongrin.com/somwthing',
+        },
+        {
+          label: 'option24',
+          url: 'https://ongrin.com/somwthing',
+        },
+        // {
+        //   label: 'option25',
+        //   url: 'https://ongrin.com/somwthing',
+        // },
+        // {
+        //   label: 'option26',
+        //   url: 'https://ongrin.com/somwthing',
+        // },
+        // {
+        //   label: 'option27',
+        //   url: 'https://ongrin.com/somwthing',
+        // },
+        // {
+        //   label: 'option28',
+        //   url: 'https://ongrin.com/somwthing',
+        // },
+        // {
+        //   label: 'option29',
+        //   url: 'https://ongrin.com/somwthing',
+        // },
+        // {
+        //   label: 'option30',
+        //   url: 'https://ongrin.com/somwthing',
+        // },
+      ]}
+    />
   )
 }
 
